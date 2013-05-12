e59f47dd0d729e0fe77229198d104e46a239df02
m-ryan <ryan@shamu.ch>
2013-05-13 00:15:54 +0200

    vsftp recipe

--- cookbooks/vsftpd/recipes/default.rb
+++ cookbooks/vsftpd/recipes/default.rb
@@ -5,8 +5,12 @@ end
 # remove the check of shell validity
 # we want the user to access only with FTP being chrooted
 # but not be able to connect with ssh
-execute "sed -e '/pam_shells/d' /etc/pam.d/vsftpd > /tmp/pam_vsftpd"
-execute 'mv /tmp/pam_vsftpd /etc/pam.d/vsftpd'
+execute "sed -e '/pam_shells/d' /etc/pam.d/vsftpd > /tmp/pam_vsftpd" do
+  only_if 'grep pam_shells /etc/pam.d/vsftpd'
+end
+execute 'mv /tmp/pam_vsftpd /etc/pam.d/vsftpd' do
+  only_if 'grep pam_shells /etc/pam.d/vsftpd'
+end
 
 user 'dl' do
   supports :manage_home => true
@@ -15,15 +19,13 @@ user 'dl' do
 end
 
 directory '/home/dl' do
-  owner 'dl'
-  group 'dl'
   mode 00555
 end
 
 directory '/home/dl/download' do
-  owner 'dl'
-  group 'dl'
-  mode 00555
+  owner 'rtorrent'
+  group 'rtorrent'
+  mode 00755
 end
 
 # mount has to be used instead of symlink because of the way users are chrooted
@@ -32,6 +34,8 @@ mount '/home/dl/download' do
   fstype 'none'
   options 'bind,r'
   action [:mount, :enable]
+  # Do not execute if its already mounted
+  not_if 'cat /proc/mounts | grep /home/dl/download'
 end
 
 vsftp_config_files = %w(vsftpd.user_list vsftpd.conf)
