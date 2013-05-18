43524957d26806bac6a4622817861131d2b44871
m-ryan <ryan@shamu.ch>
2013-05-19 00:14:10 +0200

    teamspeak recipe

--- cookbooks/teamspeak/recipes/default.rb
+++ cookbooks/teamspeak/recipes/default.rb
@@ -1,16 +1,27 @@
 user 'teamspeak' do
   supports :manage_home => true
-  home "/home/teamspeak"
+  home '/home/teamspeak'
   shell '/bin/bash'
 end
 
-remote_file "/home/teamspeak/teamspeak.tar.gz" do
-  source node[:teamspeak][:server_url]
+remote_file '/home/teamspeak/teamspeak.tar.gz' do
+  source node[:teamspeak][:download_url]
+  owner 'teamspeak'
+  group 'teamspeak'
 end
 
-execute "tar xzf teamspeak.tar.gz" do
-  cwd "/home/teamspeak"
-  user "teampseak"
+execute 'tar xzf teamspeak.tar.gz' do
+  cwd '/home/teamspeak'
+  user 'teamspeak'
+  not_if 'test -d /etc/init.d/teamspeak.sh'
+end
+
+cookbook_file '/etc/init.d/teamspeak.sh' do
+  source 'teamspeak.sh'
+end
+
+execute '/etc/init.d/teamspeak.sh start' do
+  not_if 'ps -eaf | grep ts3server_linux_amd64 | grep -v grep'
 end
 
 
